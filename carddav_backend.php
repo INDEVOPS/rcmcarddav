2a45128863a7aa7805eb6d908bcda4a404382e38
Arkadiusz Kuptz <arkadiusz.kuptz@indevops.com>
2021-07-12 11:06:57 +0000

    Revert "Fix empty address books in Easy Redmine"
    This reverts commit f2a07c69c336cf0d5d1615beed2049328491d847.

--- carddav_backend.php
+++ carddav_backend.php
@@ -859,10 +859,14 @@ EOF
 	{{{
     $records = 0;
 	$urls = array();
-	$xpresult = $xml->xpath('//d:response[d:propstat/d:status="HTTP/1.0 200 OK" and d:href]');
+	$xpresult = $xml->xpath('//RCMCD:response[starts-with(translate(child::RCMCD:propstat/RCMCD:status, "ABCDEFGHJIKLMNOPQRSTUVWXYZ", "abcdefghjiklmnopqrstuvwxyz"), "http/1.1 200 ") and child::RCMCD:propstat/RCMCD:prop/RCMCD:getetag]');
 	foreach ($xpresult as $r) {
-		$href =  (string) $r->xpath('d:href')[0];
-		$etag = (string) $r->xpath('d:propstat/d:prop/d:getetag')[0];
+		self::$helper->registerNamespaces($r);
+
+		list($href) = $r->xpath('child::RCMCD:href');
+		if(preg_match('/\/$/', $href)) continue;
+
+		list($etag) = $r->xpath('descendant::RCMCD:getetag');
 
 		$ret = self::checkcache($this->existing_card_cache,"$href","$etag");
 		$retgrp = self::checkcache($this->existing_grpcard_cache,"$href","$etag");
